b33e92fd2d3c6c069765d8bc53065b51e260ddd4
Abdul359 <abdul.layeeq359@gmail.com>
2020-10-03 15:42:44 +0530

    second commit fixed some typos

--- assets/index.js
+++ assets/index.js
@@ -1,25 +1,16 @@
-//
-// #D6e0df
-
-//#dad4d6
-
-// #e3c0ad
-
-//#cca19b
-
-//#e3d1c3
-
-
-//#e1dee9
-
+// import jump.js library
 import jump from './node_modules/jump.js/dist/jump.module.js'
+
+// sal.js function call
 sal()
+
+// event listener for Button and selector
 const navBtn = document.querySelector(".nav-btn");
 const contactMe = document.querySelector(".contact-me");
 navBtn.addEventListener("click", ()=>{
-    navBtn.classList.toggle("openNav")
-    contactMe.classList.toggle("showContact")
-})
+    navBtn.classList.toggle("openNav");
+    contactMe.classList.toggle("showContact");
+});
 
 const knownContainer = document.querySelector(".known");
 const extra = document.querySelector(".extra");
@@ -31,32 +22,35 @@ const projects = document.querySelector(".projects-link");
 const home = document.querySelector(".home-link");
 const about = document.querySelector(".about-link");
 const contact = document.querySelector(".contact-link");
-const mobileContact = document.querySelector(".contact__btn")
+const mobileContact = document.querySelector(".contact__btn");
 
+
+
+// jump.js animation listener 
 projects.addEventListener("click", ()=>{
     jump('.worked-projects', {
         duration:  3000,
-      })
-})
+    });
+});
 home.addEventListener("click", ()=>{
     jump('.showcase-content ', {
         duration:  500,
-      })
-})
+    });
+});
 about.addEventListener("click", ()=>{
     jump('.about-me', {
         duration:2000
-      })
-})
+    });
+});
 contact.addEventListener("click", ()=>{
     jump('.contact', {
         duration:4000
-      })
-})
+    });
+});
 mobileContact.addEventListener("click", ()=>{
     jump('.contact__me', {
         duration:4000
-      })
-})
+    });
+});
 
 
